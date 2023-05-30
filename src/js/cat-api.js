4a624cc1fa5e9a9704c1f25e76e63fd54bdfc365
supreme-cake-being <XGon20987@gmail.com>
2023-05-30 18:04:06 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,5 @@
 const API_KEY = 'live_p6iVI1ioCEGoQdLGYqbIXdpOS9NVRrKIZYrDUJhgKcUvqAY3mNojGEOTw9dkXtQZ';
 
-
 const fetchBreeds = (id) => {
     return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
         .then(response => response.json());

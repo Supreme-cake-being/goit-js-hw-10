b195a448892630da3475d3e9ab2f0e2563f93c11
supreme-cake-being <XGon20987@gmail.com>
2023-05-30 17:47:25 +0300

    done

---
+++ src/js/cat-api.js
@@ -0,0 +1,14 @@
+const API_KEY = 'live_p6iVI1ioCEGoQdLGYqbIXdpOS9NVRrKIZYrDUJhgKcUvqAY3mNojGEOTw9dkXtQZ';
+
+
+const fetchBreeds = (id) => {
+    return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`)
+        .then(response => response.json());
+}
+
+const fetchCatByBreed = (id) => {
+    return fetch(`https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${id}`)
+        .then(response => response.json());
+}
+
+export { fetchBreeds, fetchCatByBreed };
